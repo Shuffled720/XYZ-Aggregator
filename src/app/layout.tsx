4c489b07bb826fbec5c95f6309af1d564894d6f0
Shuffled720 <shuffled720@gmail.com>
2023-12-27 23:27:05 +0530

    toggler added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 import { GlobalContextProvider } from "@/context/store";
+import { ModeToggle } from "@/components/ModeToggle";
 export const metadata: Metadata = {
   title: "XYZ Aggregator",
   description: "Genereates JSON from user given keywords with mini browser",
@@ -26,6 +27,9 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <GlobalContextProvider>
+            <div className="fixed top-0 right-0 m-2">
+              <ModeToggle />
+            </div>
             {children}
             <Footer />
           </GlobalContextProvider>
